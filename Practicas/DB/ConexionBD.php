140ab0e254f709b4ddb520a998442894ad417a83
Unknown <16171324@itculiacan.edu.mx>
2018-03-12 23:17:07 -0700

    conexiones

--- Practicas/DB/ConexionBD.php
+++ Practicas/DB/ConexionBD.php
@@ -6,15 +6,38 @@ class ConexionBD
   private $basedatos;
   private $equipo;
   private $conexion;
-  function __construct($usuario,$contra,$basedatos,$equipo){
+  public function __construct($usuario,$contra,$basedatos,$equipo){
     $this->usuario=$usuario;
     $this->contra=$contra;
     $this->basedatos=$basedatos;
     $this->equipo=$equipo;
     $this->conexion = mysqli_connect($equipo,$usuario,$contra,$basedatos);
   }
-  function getConexion(){
+  public function getConexion(){
     return $this->conexion;
+  }
+  public function Consulta($Table, $ID, $Bus)
+  {
+    $res = $this->conexion->query("SELECT * FROM $Table WHERE $ID=$Bus");
+    $resultado= $this->conexion->query("SHOW COLUMNS FROM $Table");
+    if (!$resultado) {
+        echo 'No se pudo ejecutar la consulta';
+        exit;
+    }
+    if (mysqli_num_rows($resultado) > 0) {
+      $p=0;
+      while ($Columna = mysqli_fetch_assoc($resultado)) {
+        $array[$p] = $Columna['Field'];
+        $p++;
+      }
+      while ($fila = mysqli_fetch_assoc($res))
+      {
+        for ($i=0; $i <count($array); $i++) {
+          echo $fila[$array[$i]]."    ";
+        }
+        echo "<br>";
+      }
+    }
   }
 }
 ?>
--- Practicas/principal.php
+++ Practicas/principal.php
@@ -1,12 +1,6 @@
 <?php
-include("Db/conexion.php");
+include("Db/ConexionBD.php");
 require 'metodos/Rutinas.php';
-$con =conectar();
-echo" se realizo correctamente la conexion a la base de datos <br>";
-//Busqueda(rand(1,720));
-//Leer tabla
-//Lectura();
-Busqueda('Tipo',4);
-mysqli_close($con);
-
+$conexion= new ConexionBD("root","","agenda","localhost");
+$conexion->Consulta("agendatelefonica",'Tipo',4);
 ?>
